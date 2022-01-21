2d5c2df28484c77288f41b7410b6ba86d4a40bf4
Jose-Tejero <j_jtejero@hotmail.com>
2022-01-21 17:16:08 -0600

    Intentando usar SVG en mi imagen

--- src/App.js
+++ src/App.js
@@ -32,7 +32,20 @@ function App() {
               </button>
             </a>
           </div>
-          <img src={yo} alt="José Tejero" />
+          <div className='minibody' >
+            <svg viewbox="0 0 50 50" width="50" height="50">
+              <defs>
+                <mask id="mask" x="0" y="0" width="100" height="49">
+                  <rect x="0.5" y="0.5" width="49" height="49" fill="#fff"/>
+                  <text x="20" text-anchor="middle" y="50" dy="0">J</text>
+                  <text x="25" id="ltrV" text-anchor="middle" y="50" dy="0">T</text>
+                  <text x="30" text-anchor="middle" y="50" dy="0">Z</text>
+                </mask>
+              </defs>
+              <rect x="0.5" y="0.5" width="49" height="49" mask="url(#mask)" fill-opacity="1" fill="#E3DFD2"/>
+            </svg>
+            {/* <img src={yo} alt="José Tejero" /> */}
+          </div>
         </main>
       </div>
 
